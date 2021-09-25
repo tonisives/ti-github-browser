a7b6bf85d15e1da1be3ee46951766acdabe82d05
tonis <tonisives@gmail.com>
2021-09-25 14:50:40 +0700

    add login fields

--- app/src/main/java/com/tonisives/githubbrowser/ui/LoginActivity.kt
+++ app/src/main/java/com/tonisives/githubbrowser/ui/LoginActivity.kt
@@ -1,21 +1,32 @@
 package com.tonisives.githubbrowser.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import com.tonisives.githubbrowser.App.Companion.context
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 @Composable
-fun LoginView() = AppTheme {
+fun LoginView(viewModel: LoginViewModel) = AppTheme {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -23,20 +34,71 @@ fun LoginView() = AppTheme {
             .background(Color.LightGray)
             .fillMaxSize()
     ) {
-        Text("Hello from compose")
+        LoginFields(
+            email,
+            password,
+            onLoginClick = { viewModel.login(email, password) },
+            onEmailChange = { email = it },
+            onPasswordChange = { password = it }
+        )
+    }
+}
+
+@Composable
+fun LoginFields(
+    email: String,
+    password: String,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onLoginClick: (String) -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp),
+        verticalArrangement = Arrangement.spacedBy(25.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login")
+
+        OutlinedTextField(
+            value = email,
+            placeholder = { Text(text = "user@email.com") },
+            label = { Text(text = "email") },
+            onValueChange = onEmailChange,
+        )
+
+        OutlinedTextField(
+            value = password,
+            placeholder = { Text(text = "password") },
+            label = { Text(text = "password") },
+            onValueChange = onPasswordChange,
+        )
+
+        Button(onClick = {
+            if (email.isBlank() == false && password.isBlank() == false) {
+                onLoginClick(email)
+                focusManager.clearFocus()
+            } else {
+                Toast.makeText(
+                    context,
+                    "Please enter email and password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }) {
+            Text("Login")
+        }
     }
 }
 
 class LoginActivity : AppCompatActivity() {
-    private val viewModel: LoginViewModel by viewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginView()
+            LoginView(getViewModel())
         }
-    }
-
-    fun login() {
     }
 }
